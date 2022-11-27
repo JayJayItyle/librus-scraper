0f7bb4040389660c1ffa34c21a899afccf2d14ef
Pingwinek <91051771+JayJayItyle@users.noreply.github.com>
2022-11-27 19:58:25 +0100

    Updated config-check.js
    Translated logs to english

--- checkers/config-check.js
+++ checkers/config-check.js
@@ -12,24 +12,24 @@ function check() {
       if (port.check(WEBSERVER_PORT)) {
       
       } else {
-        console.log('blad konfuguracji - webserver_port nie jest prawidlowym portem');
+        console.log('config error - webserver_port is not a valid port');
         process.exit()
       }
       
       } else {
-        console.log('blad konfuguracji - webserver_port nie jest liczba');
+        console.log('config error - webserver_port is not a number');
         process.exit()
       }
       
       if (typeof WEBSERVER_BOOLEAN === 'boolean') {
       } else {
-        console.log('blad konfiguracji - webserver nie jest wartoscia true/false');
+        console.log('config error - webserver is not a boolean');
         process.exit()
       }
   
       if (typeof SZCZESLIWYNUMEREK_BOOLEAN === 'boolean') {
       } else {
-        console.log('blad konfiguracji - szczesliwy_numerek nie jest wartoscia true/false');
+        console.log('blad konfiguracji - szczesliwy_numerek is not a boolean');
         process.exit()
       }
       
